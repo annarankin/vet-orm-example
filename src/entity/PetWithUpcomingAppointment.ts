dc9c805b4d36e6f774fc91429e239825fbda3058
Anna Rankin <anna@annarankin.com>
2018-11-21 11:18:20 -0500

    Note in view model

--- src/entity/PetWithUpcomingAppointment.ts
+++ src/entity/PetWithUpcomingAppointment.ts
@@ -1,6 +1,8 @@
 import { Entity, Column, BaseEntity, PrimaryColumn, Timestamp, ManyToOne, JoinColumn } from 'typeorm'
 import { User } from './User'
 
+// Important! This is not a table, and we don't want TypeORM to treat
+// it as such - we tell it NOT to synchronize ths model with the DB.
 @Entity('pets_with_upcoming_appointments', { synchronize: false })
 export class PetWithUpcomingAppointment extends BaseEntity {
   @PrimaryColumn({ name: 'pet_id' })
